dbed2ecdf787f27d57d3955ebe83a98e316d0a3d
Michael Hatmaker <michaeldhatmaker@gmail.com>
2020-03-06 22:57:11 -0600

    PWA (Progressive Web App)
    I love the PWA concept, but it seems poorly explained and poorly documented. Here  we worked through a first PWA example.

---
+++ PWA-Web-App/PWA-Web-App/routes/index.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+// https://docs.microsoft.com/en-us/microsoft-edge/progressive-web-apps-edgehtml/get-started
+// https://itnext.io/pwa-splash-screen-and-icon-generator-a74ebb8a130
+// https://www.pwabuilder.com/
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Introduction
+// https://developers.google.com/web/tools/puppeteer/
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
